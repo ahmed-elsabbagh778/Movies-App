a68bea472fce5fed63c7f8a6ae72eb296b6a1826
ahmed-elsabbagh778 <ahmedelsabbagh973@gmail.com>
2025-05-12 17:29:33 +0300

    removed duplicate import

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -7,7 +7,6 @@ import { addRemoveMovie } from "../../store/slices/watchList";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 function MovieCard(props) {
   const { movie } = props;
