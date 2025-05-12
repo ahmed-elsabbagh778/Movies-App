afe927404b9673c9a32031bd9eb299f90c2441ff
ahmed-elsabbagh778 <ahmedelsabbagh973@gmail.com>
2025-05-13 01:15:37 +0300

    added function handleClick for movie card + Navigation to movie details

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -7,14 +7,20 @@ import { addRemoveMovie } from "../../store/slices/watchList";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function MovieCard(props) {
   const { movie } = props;
+  const navigate = useNavigate();
+  const apiKey = import.meta.env.VITE_APP_API_KEY;
 
   const watchList = useSelector((state) => state.watchList.moviesWatchList);
 
   const isInWatchList = watchList.some((item) => item.id === movie.id);
+
+  const handleClick = () => {
+    navigate(`/movie/${movie.id}?api_key=${apiKey}`);
+  };
 
   const dispatch = useDispatch();
   const handleWatchList = () => {
@@ -23,7 +29,7 @@ function MovieCard(props) {
 
   return (
     <Card className="movie-card">
-      <div className="image-container" onClick={() => alert("clicked")}>
+      <div className="image-container" onClick={handleClick}>
       <Link to={`/movie/${movie.id}`}>
         <Card.Img
         className="movie-card-img"
