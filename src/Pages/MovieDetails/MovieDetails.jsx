81cedee7598dd032de092ebe8a0a5dd3cd55d6ce
ahmed-elsabbagh778 <ahmedelsabbagh973@gmail.com>
2025-05-14 02:41:16 +0300

    added production company logo to details

--- src/Pages/MovieDetails/MovieDetails.jsx
+++ src/Pages/MovieDetails/MovieDetails.jsx
@@ -14,78 +14,104 @@ const MovieDetails = () => {
   const apiKey = import.meta.env.VITE_APP_API_KEY;
   const { language } = useLanguage();
 
-useEffect(() => {
-  window.scrollTo(0, 0);
-  axiosInstance
-    .get(`/movie/${id}?api_key=${apiKey}&language=${language}`)
-    .then((res) => setMovie(res.data))
-    .catch((err) => console.log(err));
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    axiosInstance
+      .get(`/movie/${id}?api_key=${apiKey}&language=${language}`)
+      .then((res) => setMovie(res.data))
+      .catch((err) => console.log(err));
 
-  axiosInstance
-    .get(`/movie/${id}/recommendations?api_key=${apiKey}&language=${language}`)
-    .then((res) => setRecommendations(res.data.results))
-    .catch((err) => console.log(err));
-}, [id, language]);
-
+    axiosInstance
+      .get(
+        `/movie/${id}/recommendations?api_key=${apiKey}&language=${language}`
+      )
+      .then((res) => setRecommendations(res.data.results))
+      .catch((err) => console.log(err));
+  }, [id, language]);
 
   if (!movie) return <LoadingSpinner />;
   return (
     <>
-    <div className="movie-details container-fluid px-5">
-       <div className="row align-items-center">
-      <div className="col-md-4 poster-container">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-            className="poster-img img-fluid"
-          />
-        </div>
-        <div className="col-md-8 info-container">
-        <h2 className="movie-title">{movie.title}</h2>
-        <p className="movie-description">{movie.overview}</p>
-          <div className="genres">
-            {movie.genres.map((genre) => (
-              <span className="genre-badge" key={genre.id}>
-                {genre.name}
-              </span>
-            ))}
+      <div className="movie-details container-fluid px-5">
+        <div className="row align-items-center">
+          <div className="col-md-3 poster-container">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className="poster-img img-fluid"
+            />
           </div>
-          <p><strong>Release Date:</strong> {movie.release_date}</p>
-          <p><strong>Rating:</strong> {movie.vote_average}</p>
-          <div className="details">
-            <p><strong>Duration:</strong> {movie.runtime} Min.</p>
-            <p><strong>Languages:</strong> {movie.original_language}</p>
-          </div>
-        </div>
-      </div>
-      </div>
-  {recommendations.length > 0 && (
-  <div className="container-fluid px-5 mt-5">
-    <h3 className="recommendations-title text-white mb-4">Recommendations</h3>
-    <Carousel indicators={false} interval={null}>
-      {recommendations.slice(0, 20).reduce((groups, rec, index) => {
-        const groupIndex = Math.floor(index / 5);
-        if (!groups[groupIndex]) groups[groupIndex] = [];
-        groups[groupIndex].push(rec);
-        return groups;
-      }, []).map((group, idx) => (
-        <Carousel.Item key={idx}>
-          <div
-            className="d-flex justify-content-center gap-4"
-            style={{ flexWrap: "nowrap", overflowX: "auto" }}
-          >
-            {group.map((rec) => (
-            <div key={rec.id} style={{ minWidth: "200px" }}>
-              <MovieCard movie={rec} />
+          <div className="col-md-8 info-container">
+            <h2 className="movie-title">{movie.title}</h2>
+            <p className="movie-description">{movie.overview}</p>
+            <div className="genres">
+              {movie.genres.map((genre) => (
+                <span className="genre-badge" key={genre.id}>
+                  {genre.name}
+                </span>
+              ))}
             </div>
-          ))}
+            <p>
+              <strong>Release Date:</strong> {movie.release_date}
+            </p>
+            <p>
+              <strong>Rating:</strong> {movie.vote_average}
+            </p>
+            <div className="details">
+              <p>
+                <strong>Duration:</strong> {movie.runtime} Min.
+              </p>
+              <p>
+                <strong>Languages:</strong> {movie.original_language}
+              </p>
+              {movie.production_companies.map(
+                (company) =>
+                  company.logo_path && (
+                    <div key={company.id} className="production-logo mt-4">
+                      <img
+                        src={`https://image.tmdb.org/t/p/w200${company.logo_path}`}
+                        alt={company.name}
+                        style={{ maxHeight: "50px", marginRight: "10px" }}
+                      />
+                    </div>
+                  )
+              )}
+            </div>
           </div>
-        </Carousel.Item>
-      ))}
-    </Carousel>
-  </div>
-)}
-</>
+        </div>
+      </div>
+      {recommendations.length > 0 && (
+        <div className="container-fluid px-5 mt-5">
+          <h3 className="recommendations-title text-white mb-4">
+            Recommendations
+          </h3>
+          <Carousel indicators={false} interval={null}>
+            {recommendations
+              .slice(0, 20)
+              .reduce((groups, rec, index) => {
+                const groupIndex = Math.floor(index / 5);
+                if (!groups[groupIndex]) groups[groupIndex] = [];
+                groups[groupIndex].push(rec);
+                return groups;
+              }, [])
+              .map((group, idx) => (
+                <Carousel.Item key={idx}>
+                  <div
+                    className="d-flex justify-content-center gap-4"
+                    style={{ flexWrap: "nowrap", overflowX: "auto" }}
+                  >
+                    {group.map((rec) => (
+                      <div key={rec.id} style={{ minWidth: "200px" }}>
+                        <MovieCard movie={rec} />
+                      </div>
+                    ))}
+                  </div>
+                </Carousel.Item>
+              ))}
+          </Carousel>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/Pages/TVshowsDetails/TVshowsDetails.jsx
+++ src/Pages/TVshowsDetails/TVshowsDetails.jsx
@@ -14,78 +14,103 @@ const TVshowsDetails = () => {
   const apiKey = import.meta.env.VITE_APP_API_KEY;
   const { language } = useLanguage();
 
-useEffect(() => {
-  window.scrollTo(0, 0); // scroll to the top of page
-  axiosInstance
-    .get(`/tv/${id}?api_key=${apiKey}&language=${language}`)
-    .then((res) => setTvShows(res.data))
-    .catch((err) => console.log(err));
+  useEffect(() => {
+    window.scrollTo(0, 0); // scroll to the top of page
+    axiosInstance
+      .get(`/tv/${id}?api_key=${apiKey}&language=${language}`)
+      .then((res) => setTvShows(res.data))
+      .catch((err) => console.log(err));
 
-  axiosInstance
-    .get(`/tv/${id}/recommendations?api_key=${apiKey}&language=${language}`)
-    .then((res) => setRecommendations(res.data.results))
-    .catch((err) => console.log(err));
-}, [id, language]);
-
+    axiosInstance
+      .get(`/tv/${id}/recommendations?api_key=${apiKey}&language=${language}`)
+      .then((res) => setRecommendations(res.data.results))
+      .catch((err) => console.log(err));
+  }, [id, language]);
 
   if (!tvShows) return <LoadingSpinner />;
   return (
     <>
-    <div className="tvShows-details container-fluid px-5">
-       <div className="row align-items-center">
-      <div className="col-md-4 poster-container">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${tvShows.poster_path}`}
-            alt={tvShows.name}
-            className="poster-img img-fluid"
-          />
-        </div>
-        <div className="col-md-8 info-container">
-        <h2 className="tvShows-name">{tvShows.name}</h2>
-        <p className="tvShows-description">{tvShows.overview}</p>
-          <div className="genres">
-            {tvShows.genres.map((genre) => (
-              <span className="genre-badge" key={genre.id}>
-                {genre.name}
-              </span>
-            ))}
+      <div className="tvShows-details container-fluid px-5">
+        <div className="row align-items-center">
+          <div className="col-md-4 poster-container">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${tvShows.poster_path}`}
+              alt={tvShows.name}
+              className="poster-img img-fluid"
+            />
           </div>
-          <p><strong>Release Date:</strong> {tvShows.first_air_date}</p>
-          <p><strong>Rating:</strong> {tvShows.vote_average}</p>
-          <div className="details">
-            <p><strong>Duration:</strong> {tvShows.episode_run_time?.[0] || "N/A"} Min.</p>
-            <p><strong>Languages:</strong> {tvShows.original_language}</p>
-          </div>
-        </div>
-      </div>
-      </div>
-  {recommendations.length > 0 && (
-  <div className="container-fluid px-5 mt-5">
-    <h3 className="recommendations-name text-white mb-4">Recommendations</h3>
-    <Carousel indicators={false} interval={null}>
-      {recommendations.slice(0, 20).reduce((groups, rec, index) => {
-        const groupIndex = Math.floor(index / 5);
-        if (!groups[groupIndex]) groups[groupIndex] = [];
-        groups[groupIndex].push(rec);
-        return groups;
-      }, []).map((group, idx) => (
-        <Carousel.Item key={idx}>
-          <div
-            className="d-flex justify-content-center gap-4"
-            style={{ flexWrap: "nowrap", overflowX: "auto" }}
-          >
-            {group.map((rec) => (
-            <div key={rec.id} style={{ minWidth: "200px" }}>
-              <TVShowsCard tvShow={rec} />
+          <div className="col-md-8 info-container">
+            <h2 className="tvShows-name">{tvShows.name}</h2>
+            <p className="tvShows-description">{tvShows.overview}</p>
+            <div className="genres">
+              {tvShows.genres.map((genre) => (
+                <span className="genre-badge" key={genre.id}>
+                  {genre.name}
+                </span>
+              ))}
             </div>
-          ))}
+            <p>
+              <strong>Release Date:</strong> {tvShows.first_air_date}
+            </p>
+            <p>
+              <strong>Rating:</strong> {tvShows.vote_average}
+            </p>
+            <div className="details">
+              <p>
+                <strong>Duration:</strong>{" "}
+                {tvShows.episode_run_time?.[0] || "N/A"} Min.
+              </p>
+              <p>
+                <strong>Languages:</strong> {tvShows.original_language}
+              </p>
+              {tvShows.production_companies.map(
+                (company) =>
+                  company.logo_path && (
+                    <div key={company.id} className="production-logo mt-4">
+                      <img
+                        src={`https://image.tmdb.org/t/p/w200${company.logo_path}`}
+                        alt={company.name}
+                        style={{ maxHeight: "50px", marginRight: "10px" }}
+                      />
+                    </div>
+                  )
+              )}
+            </div>
           </div>
-        </Carousel.Item>
-      ))}
-    </Carousel>
-  </div>
-)}
-</>
+        </div>
+      </div>
+      {recommendations.length > 0 && (
+        <div className="container-fluid px-5 mt-5">
+          <h3 className="recommendations-name text-white mb-4">
+            Recommendations
+          </h3>
+          <Carousel indicators={false} interval={null}>
+            {recommendations
+              .slice(0, 20)
+              .reduce((groups, rec, index) => {
+                const groupIndex = Math.floor(index / 5);
+                if (!groups[groupIndex]) groups[groupIndex] = [];
+                groups[groupIndex].push(rec);
+                return groups;
+              }, [])
+              .map((group, idx) => (
+                <Carousel.Item key={idx}>
+                  <div
+                    className="d-flex justify-content-center gap-4"
+                    style={{ flexWrap: "nowrap", overflowX: "auto" }}
+                  >
+                    {group.map((rec) => (
+                      <div key={rec.id} style={{ minWidth: "200px" }}>
+                        <TVShowsCard tvShow={rec} />
+                      </div>
+                    ))}
+                  </div>
+                </Carousel.Item>
+              ))}
+          </Carousel>
+        </div>
+      )}
+    </>
   );
 };
 
